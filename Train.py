36a02bc767c38f2af980639849c57ae050871454
Ubuntu <ubuntu@ip-172-31-36-45.us-west-2.compute.internal>
2023-05-18 08:52:17 +0000

    Added image and bbox plots per batch saved to image_storage folder, currently, bbox are not well defined, so I just plotted 4 points, of which we have to interpret the location of the bbox. Additionally, built framework of run_epochs function, and made print statements for the multiple loss functions we have.

--- Train.py
+++ Train.py
@@ -1,7 +1,10 @@
+from __future__ import print_function
 import os
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import torch.optim as optim
 from torch.utils.data import DataLoader
@@ -14,6 +17,8 @@ from AngioDataset import MyDataset
 from utils import *
 import torchvision
 
+
+#plt.ioff()
 
 #load the model
 backbone = "Faster RCNN Resnet 50"
@@ -38,23 +43,32 @@ optimizer = optim.SGD(FRCNN.parameters(), lr=0.005, momentum=0.9, weight_decay=0
 
 print("starting the training")
 
-#Training loo
+# Training loop
 num_epochs = 1
 for epoch in range(num_epochs):
-    print(epoch)
+    print("Epoch:", epoch)
     for i, data in enumerate(angio_data):
+        name = 'image_storage/test' + str(i) + '.png'
         _, inpt, label = data
+        points = label[0]
+        plt.figure()
+        plt.imshow(inpt[0,:,:,:])  # the first dimension is of size 2, but i wasn't sure whether to go with 0 or 1, as 0 looked fine
+        plt.scatter([points[0][0].item(), points[1][0].item(), points[2][0].item(), points[3][0].item()], [points[0][1].item(), points[1][1].item(), points[2][1].item(), points[3][1].item()])
+        plt.savefig(name)
         images,targets = preprocess(inpt,label)
         images = list(image.to(device) for image in images)
         targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
         optimizer.zero_grad()
+        
         loss_dict = FRCNN(images, targets)
-        print(loss_dict)
+        print('Classifier_Loss:', loss_dict['loss_classifier'].item(), 'BBox_Reg_Loss:', loss_dict['loss_box_reg'].item(), 'RPN_BBox_Reg_Loss:', loss_dict['loss_rpn_box_reg'].item(), 'Objectness_Loss:', loss_dict['loss_objectness'].item())
+        
         losses = sum(loss for loss in loss_dict.values())
         losses.backward()
         optimizer.step()
    
-        print(losses.item())
+        print('Net Loss', losses.item())
 
 print("Finished Training")
 
+    
