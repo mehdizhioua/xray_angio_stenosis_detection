fd1ae8f774e9e8217503bae0d16bfc740decc6e7
Mehdi Zhioua <mehdiz@stanford.edu>
2023-06-01 03:35:21 +0000

    all set

--- AngioDataset.py
+++ AngioDataset.py
@@ -6,6 +6,7 @@ from torchvision import transforms, utils
 import xml.etree.ElementTree as ET
 import os
 import cv2
+from torch.utils.data.dataset import random_split
 
 
 def parse_annotation(xml_path):
@@ -54,34 +55,22 @@ def get_data_loader(img_path,ann_path,batch=3):
     return data_loader
 
 
+def get_train_dev_test(img_path, ann_path, batch=3, dev_split=0.15, test_split=0.15):
+    # Create the full dataset
+    dataset = AngioData(img_dir=img_path, xml_dir=ann_path)
 
-#def parse_annotation(xml_path):
-#    tree = ET.parse(xml_path)
-#    root = tree.getroot()
-#    bboxes = []
-#    for obj in root.findall('object'):
-#        bbox = obj.find('bndbox')
-#        bboxes.append([int(bbox.find('xmin').text), int(bbox.find('ymin').text),
-#                       int(bbox.find('xmax').text), int(bbox.find('ymax').text)])
-#    return bboxes
+    # Compute the lengths for the train/dev/test sets
+    test_len = int(test_split * len(dataset))
+    dev_len = int(dev_split * len(dataset))
+    train_len = len(dataset) - dev_len - test_len
 
-#class MyDataset(torch.utils.data.Dataset):
-#    def __init__(self, img_dir, xml_dir):
-#        self.image_dir = img_dir
-#        self.xml_dir = xml_dir
-#        self.image_list = os.listdir(img_dir)
+    # Randomly split the dataset
+    train_dataset, dev_dataset, test_dataset = random_split(dataset, [train_len, dev_len, test_len])
 
-#    def __len__(self):
-#        return len(self.image_list)
+    # Create the data loaders
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch, collate_fn=collate_fn)
+    dev_loader = torch.utils.data.DataLoader(dev_dataset, batch_size=batch, collate_fn=collate_fn)
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch, collate_fn=collate_fn)
 
-#    def __getitem__(self, idx):
-#        # load image
-#        image_name = self.image_list[idx]
-#        image_path = os.path.join(self.image_dir, image_name)
-#        images = cv2.imread(image_path)
+    return train_loader, dev_loader, test_loader
 
-#        # parse the XML file and extract the desired information        
-#        xml_path = os.path.join(self.xml_dir, image_name.replace('.bmp', '.xml'))
-#        bboxes = parse_annotation(xml_path)
-        
-#        return image_name, images, bboxes
--- Train2.py
+++ Train2.py
@@ -24,15 +24,11 @@ torch.cuda.empty_cache()
 
 
 #loading the data
-img_dir = 'big_data/image_dir'
-xml_dir = 'big_data/xml_dir'
-angio_data = get_data_loader(img_dir,xml_dir,batch=7)
+img_dir = 'data/image_dir'
+xml_dir = 'data/xml_dir'
+train_loader, dev_loader, test_loader = get_train_dev_test(img_dir, xml_dir, batch=7, dev_split=0.15, test_split=0.15)
 
-
-print("DATA LOADED")
-
-
-
+print("--------DATA LOADED--------")
 #loading the model
 backbone = "Faster RCNN Resnet 50"
 detector = StenosisDetector(backbone)
@@ -44,54 +40,67 @@ optimizer = optim.SGD(FRCNN.parameters(), lr=0.005, momentum=0.9, weight_decay=0
 
 writer = SummaryWriter("runs/may_31")
 
-def run_epochs(num_epochs,writer_,model,n_epoch,total_batches_=0):
+def run_epochs(num_epochs, model, train_data, dev_data, total_batches_=0):
     """
     Does the Training loop below but with multiple epochs? Will need further modification.
     """
     for epoch in range(num_epochs):
-        for i, data in enumerate(angio_data):
+        # Training step
+        model.train()
+        for i, data in enumerate(train_data):
             inpt, label = data
             img_i = preprocess_img(inpt,device)
             target_i = preprocess_label(label,device)
             if i%300==0 and i>0:
                 visualize(FRCNN,img_i,target_i,"train_"+str(i)+"_epoch_"+str(n_epoch)+".png")
             optimizer.zero_grad()
-            loss_dict = FRCNN(img_i, target_i)
+            loss_dict = model(img_i, target_i)
             losses = sum(loss for loss in loss_dict.values())
             losses.backward()
             optimizer.step()
             
             #keep track of losses 
-            writer_.add_scalar("Loss/train", losses.item(), total_batches_)
-            writer_.add_scalar("Classifier_Loss/train", loss_dict['loss_classifier'].item(), total_batches_)
-            writer_.add_scalar("BBox_Reg_Loss/train",loss_dict['loss_box_reg'].item(), total_batches_)
-            writer_.add_scalar("RPN_BBox_Reg_Loss/train", loss_dict['loss_rpn_box_reg'].item(), total_batches_)
-            writer_.add_scalar("Objectness_Loss/train",loss_dict['loss_objectness'].item(), total_batches_)
+            writer.add_scalar("Train/Loss", losses.item(), total_batches_)
+            writer.add_scalar("Train/Classifier_Loss", loss_dict['loss_classifier'].item(), total_batches_)
+            writer.add_scalar("Train/BBox_Reg_Loss",loss_dict['loss_box_reg'].item(), total_batches_)
+            writer.add_scalar("Train/RPN_BBox_Reg_Loss", loss_dict['loss_rpn_box_reg'].item(), total_batches_)
+            writer.add_scalar("Train/Objectness_Loss",loss_dict['loss_objectness'].item(), total_batches_)
             total_batches_+=1
-            
-    return total_batches_   
+
+        # dev set
+        with torch.no_grad():
+            total_loss = 0
+            for i, data in enumerate(dev_data):
+                inpt, label = data
+                img_i = preprocess_img(inpt,device)
+                target_i = preprocess_label(label,device)
+                loss_dict = model(img_i, target_i)
+                total_loss += sum(loss for loss in loss_dict.values()).item()
+            avg_loss = total_loss / len(dev_data)
+            writer.add_scalar("Dev/Loss", avg_loss, epoch)
+
+    return total_batches_
 
 
-def train(N_epoch):
+
+def train(N_epoch, train_data, dev_data):
     total_batches = 0
     print("Device",device)
     print("starting the training")
     for epch in range(N_epoch):
         print("epoch",epch)
-        inpt_e, label_e = next(iter(angio_data))
+        inpt_e, label_e = next(iter(train_data))
         img_e = preprocess_img(inpt_e,device)
         target_e = preprocess_label(label_e,device)
         visualize(FRCNN,img_e,target_e,"test"+str(epch)+".png")
-        total_btches = run_epochs(1,writer,FRCNN,epch,total_batches)
+        total_btches = run_epochs(1, FRCNN, train_data, dev_data, total_batches)
         total_batches+=total_btches
     print("Finished training")
 
     
-    
 writer.close()
 
-train(5)
+train(3, train_loader, dev_loader)
 
 #save the model    
-torch.save(FRCNN, "saved_models/fast_rcnn_resnet_50_5_epch.pth")
-
+torch.save(FRCNN, "saved_models/fast_rcnn_resnet_may31.pth")
